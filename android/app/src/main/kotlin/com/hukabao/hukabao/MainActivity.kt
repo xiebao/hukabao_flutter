5b66c876f54501f772bf3aeb853b827e0fcdcc6d
xiebaoxin <xiebaoxin@gmail.com>
2019-04-03 15:23:37 +0800

    安卓的

---
+++ android/app/src/main/kotlin/com/hukabao/hukabao/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.hukabao.hukabao
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  private val CHANNEL = "com.hukabao.flutter.xiebaoxin"
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+    MethodChannel(flutterView, CHANNEL).setMethodCallHandler { methodCall, result -> }
+  }
+}
